38d2e13df25da15168405ad93bbc0c6eab881b01
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-03-25 18:06:45 +0000

    Added com.sh file to allow downloading things from Google Drive straight

---
+++ com.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+fileid="10qN7OUzTutCg8ruwFosml0xzi0elsI5N"
+filename="train.zip"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
